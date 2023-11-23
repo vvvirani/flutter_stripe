0ce43d6c556e0333218f67edf1dc4e8ae54abfd9
Vishal Virani <55572605+vvvirani@users.noreply.github.com>
2023-11-23 11:06:03 +0530

    fix: handle null safety for `Address` class

--- lib/src/models/payment_method/payment_method.dart
+++ lib/src/models/payment_method/payment_method.dart
@@ -84,20 +84,20 @@ class BillingDetails {
 }
 
 class Address {
-  final String city;
-  final String country;
-  final String line1;
-  final String line2;
-  final String postalCode;
-  final String state;
+  final String? city;
+  final String? country;
+  final String? line1;
+  final String? line2;
+  final String? postalCode;
+  final String? state;
 
   const Address({
-    required this.city,
-    required this.country,
-    required this.line1,
-    required this.line2,
-    required this.postalCode,
-    required this.state,
+    this.city,
+    this.country,
+    this.line1,
+    this.line2,
+    this.postalCode,
+    this.state,
   });
 
   factory Address.fromMap(Map<String, dynamic> map) {
